11e67e0399349633336c24b25868f0c6ab582afc
Mikhail Guskov <meguskov@gmail.com>
2018-10-30 16:03:04 +0300

    удалил bootstrap, минифицировал js и css

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,7 @@
 const gulp = require('gulp');
-const cssmin = require('gulp-cssmin');
 const uglify = require('gulp-uglify-es').default;
 const rename = require('gulp-rename');
-
-gulp.task('css', () => {
-  return gulp
-    .src('styles.css')
-    .pipe(cssmin())
-    .pipe(rename({suffix: '.min'}))
-    .pipe(gulp.dest('.'));
-});
+var nano = require('gulp-cssnano');
 
 gulp.task('js', () => {
   return gulp
@@ -17,6 +9,13 @@ gulp.task('js', () => {
     .pipe(uglify())
     .pipe(rename({suffix: '.min'}))
     .pipe(gulp.dest('.'));
+});
+
+gulp.task('css', function () {
+  return gulp.src('styles.css')
+      .pipe(nano())
+      .pipe(rename({suffix: '.min'}))
+      .pipe(gulp.dest('.'));
 });
 
 gulp.task('default', ['css', 'js']);
