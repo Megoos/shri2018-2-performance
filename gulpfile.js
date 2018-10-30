cfb1191d96ee1a096438af29bd4f98966e9f8693
Mikhail Guskov <meguskov@gmail.com>
2018-10-30 15:06:24 +0300

    minify css and js

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,22 @@
-const gulp = require('gulp')
-const csso = require('gulp-csso'),
-const uglifyJS = require('gulp-uglify')
-
+const gulp = require('gulp');
+const cssmin = require('gulp-cssmin');
+const uglify = require('gulp-uglify-es').default;
+const rename = require('gulp-rename');
 
 gulp.task('css', () => {
-  return gulp.src('styles.css')
-    .pipe(csso())
-    .pipe(gulp.dest('./styles/'));
+  return gulp
+    .src('styles.css')
+    .pipe(cssmin())
+    .pipe(rename({suffix: '.min'}))
+    .pipe(gulp.dest('.'));
 });
 
 gulp.task('js', () => {
-  return gulp.src('scripts.js')
-    .pipe(uglifyJS())
-    .pipe(gulp.dest('./scripts/'));
+  return gulp
+    .src('scripts.js')
+    .pipe(uglify())
+    .pipe(rename({suffix: '.min'}))
+    .pipe(gulp.dest('.'));
 });
-
 
 gulp.task('default', ['css', 'js']);
